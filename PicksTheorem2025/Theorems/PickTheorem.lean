1e2f928f161e7dd0bc38bcce40e30e91396b3eba
Jonathan Langer <jonathan_f_langer@yahoo.de>
2026-02-05 00:14:35 +0100

    Klasse AngleMeasure hinzugefügt

---
+++ PicksTheorem2025/Theorems/PickTheorem.lean
@@ -0,0 +1,10 @@
+import PicksTheorem2025.Theorems.PickLemma
+-- import PicksTheorem2025.Definitions.angleMeasure
+
+--
+
+theorem jordan_curve_theorem : True := sorry
+
+theorem umlaufsatz : True := sorry
+
+theorem pick_theorem : True := sorry
